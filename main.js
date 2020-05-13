dc9ce43892fc99713b0c3eac51f363d279df56d1
dorinereijersen <45545352+dorinereijersen@users.noreply.github.com>
2020-05-13 20:15:54 +0200

    Z bij hover draaien

---
+++ main.js
@@ -0,0 +1,24 @@
+/*eslint-env browser*/
+//$(document).ready(function () {
+//    TweenMax.to($('#3hPijl'), 3, {
+//        x: 250,
+//        ease: Linear.easeInOutQuint,
+//        yoyoEase: true,
+//        repeat: -1
+//    })
+//};
+//
+//var rotate = document.getElementById('Z');
+//
+//rotate.addEventListener("click", function(){
+//  rotate.style.animation = "rotate 1s";
+//  rotate.style.webkitAnimation = "rotate 1s";
+//});
+//
+//$(document).ready(function () {
+//    alert("document.ready");
+//});
+//
+//$('#Z').click(function(){
+//    $(this).toggleClass('rotate');
+//});
