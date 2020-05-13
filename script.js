a20cc670751c6b44780bd9f3397c633a8d307f12
dorinereijersen <45545352+dorinereijersen@users.noreply.github.com>
2020-05-13 17:07:21 +0200

    pijlen laten bewegen

---
+++ script.js
@@ -0,0 +1,8 @@
+//$(document).ready(function () {
+//    TweenMax.to($('#3hPijl'), 3, {
+//        x: 250,
+//        ease: Linear.easeInOutQuint,
+//        yoyoEase: true,
+//        repeat: -1
+//    })
+//};
