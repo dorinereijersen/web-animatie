bdf19778836b85239564914fa4b6309a17a57297
dorinereijersen <45545352+dorinereijersen@users.noreply.github.com>
2020-05-14 15:16:35 +0200

    pijl moet muis volgen maar ontwijkt nog

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
 /*eslint-env browser*/
-//$(document).ready(function () {
-//    TweenMax.to($('#3hPijl'), 3, {
-//        x: 250,
-//        ease: Linear.easeInOutQuint,
-//        yoyoEase: true,
-//        repeat: -1
-//    })
-//};
+
+//https://www.youtube.com/watch?v=AixAmLWzXYg >tutorial
+
+var move = document.getElementsByClassName("pijl");
+document.onmousemove = function(){
+    var x = event.clientX * 100 / window.innerWidth + "%";
+    var y = event.clientY * 100 / window.innerHeight + "%";
+    //event.clientX => get the horizontal coordinate of the mousemove
+    //event.clientY => get the vertical coordinate of the mousemove
+    //window.innerWidth => get the browser width
+    //window.innerHeight => get the browser heigth
+    
+    for(var i=0;i<2;i++){
+        move[i].style.left = x;
+        move[i].style.top = y;
+        move[i].style.transform = "translate(-"+x+",-"+y+")";
+    }
+}
 //
-//var rotate = document.getElementById('Z');
-//
-//rotate.addEventListener("click", function(){
-//  rotate.style.animation = "rotate 1s";
-//  rotate.style.webkitAnimation = "rotate 1s";
-//});
-//
-//$(document).ready(function () {
-//    alert("document.ready");
-//});
-//
-//$('#Z').click(function(){
-//    $(this).toggleClass('rotate');
-//});
+
+
+//function onMouseMove (e) {
+//    var x = e.clientX || e.touches[0].clientX;
+//    var y = e.clientY || e.touches[0].clientY;
+//    
+//    worldYangle = -1( .5 - ( x / window.innerWidth)) *100;
+//    worldXangle = -1( .5 - ( x / window.innerHeight)) *100;
+//    updateView();
+//    event.preventDefault();
+//}
