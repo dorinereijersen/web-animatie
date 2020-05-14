967ea0de985e8873c1d0bd5396612d09cdad9f1a
dorinereijersen <45545352+dorinereijersen@users.noreply.github.com>
2020-05-14 20:17:11 +0200

    media queries?

--- main.js
+++ main.js
@@ -2,21 +2,19 @@
 
 //https://www.youtube.com/watch?v=AixAmLWzXYg >tutorial
 
-var move = document.getElementsByClassName("pijl");
-document.onmousemove = function(){
-    var x = event.clientX * 100 / window.innerWidth + "%";
-    var y = event.clientY * 100 / window.innerHeight + "%";
-    //event.clientX => get the horizontal coordinate of the mousemove
-    //event.clientY => get the vertical coordinate of the mousemove
-    //window.innerWidth => get the browser width
-    //window.innerHeight => get the browser heigth
-    
-    for(var i=0;i<2;i++){
-        move[i].style.left = x;
-        move[i].style.top = y;
-        move[i].style.transform = "translate(-"+x+",-"+y+")";
-    }
-}
+//var move = document.getElementsByClassName("pijl");
+//document.onmousemove = function(){
+//    var x = event.clientX * 100 / window.innerWidth + "%";
+//    var y = event.clientY * 100 / window.innerHeight + "%";
+//    //event.clientX => get the horizontal coordinate of the mousemove
+//    //event.clientY => get the vertical coordinate of the mousemove
+//    //window.innerWidth => get the browser width
+//    //window.innerHeight => get the browser heigth
+//    
+//    for(var i=0;i<2;i++){
+//        move[i].style.transform = "translate(+"+x+",+"+y+")";
+//    }
+//}
 //
 
 
