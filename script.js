5e33be45e02d52c47f85a916012c7f611c91d3b2
dorinereijersen <45545352+dorinereijersen@users.noreply.github.com>
2020-05-22 13:50:29 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-//$(document).ready(function () {
-//    TweenMax.to($('#3hPijl'), 3, {
-//        x: 250,
-//        ease: Linear.easeInOutQuint,
-//        yoyoEase: true,
-//        repeat: -1
-//    })
-//};
